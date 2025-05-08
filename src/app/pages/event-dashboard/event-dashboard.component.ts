2c9df16e0751541153f525ad2ebd421b67e9d976
Ivaldo Souza <ivaldo.souza@datapar.com.br>
2025-05-08 09:30:34 -0300

    Iniciando projeto: organização de pastas

---
+++ src/app/pages/event-dashboard/event-dashboard.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-event-dashboard',
+  imports: [],
+  templateUrl: './event-dashboard.component.html',
+  styleUrl: './event-dashboard.component.css'
+})
+export class EventDashboardComponent {
+
+}
